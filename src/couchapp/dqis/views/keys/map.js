7e9fc6cf3a02aab5f11014683db1963623cce716
kostast <kostast@4525493e-7705-40b1-a816-d608a930855b>
2010-04-15 14:58:55 +0000

    map meta field hidden from view
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DQIS/trunk@11 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/dqis/views/keys/map.js
+++ src/couchapp/dqis/views/keys/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   for (k in doc.map){
-    if (doc.map[k]!=null){
+    if ((doc.map[k]!=null) && (k!= "_meta")){
       var tmp =[k, doc.run, doc.map[k]];
       emit(tmp, 1);
     }
