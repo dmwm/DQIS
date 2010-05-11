bd861954c2531df1bd2e9dceb2585b9acd4cbbdc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-11 19:30:33 +0000

    New search list
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DQIS/trunk@2390 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/dqis/lists/crab.js
+++ src/couchapp/dqis/lists/crab.js
@@ -1,24 +1,51 @@
+// !code _attachments/js/json2.js
 function(head, req){
-  send('{\n');
   var row;
+  send('{');
+  var body = JSON.parse(req.body);
+  var run;
+  run = 0;
+  var lumis;
+  lumis = {};
+  var first;
+  first = 0;
   while(row = getRow()) {
-    //send(row.value);
-	  send('\"' + row.key + '\":[');
-	  var v;
-	  for each (v in row.value){
-		  send('[' + v + ',' + v + '],');
-	  }
-	  send('],\n');
+  	if (row.value.run != run) {
+  		// We've changed run, send the lumis for the last run
+  		if (lumis.length > 0){
+  		  distinct_lumis = [];
+  		  // Can you get all the keys easily?
+			  for (var k in lumis){
+			    distinct_lumis.push(parseInt(k));
+			  }
+			  
+    	  send('"' + run + '":' + toJSON(distinct_lumis.sort()));
+			  
+        if (first > 1) {
+          send(', ');
+        } else {
+          first += 1;
+        }
+  		}
+  		run = row.value.run;
+  		lumis = [];
+  	}
+  	var sendRow;
+  	// If this was set to true the queries would be inclusive - should be an option
+  	sendRow = false;
+  	if (body.bfield) {
+  		sendRow = (parseInt(body.bfield) == parseInt(row.value.bfield));
+  	}
+  	// If we get a map we'd better make sure the doc matches the keys
+  	if (body.map) {
+  		for (key in body.map) {
+  			sendRow = (body.map[key] == row.value.map[key]);
+  		}
+  	}
+  	if (sendRow == true) {
+  	  // Couldn't this be a check for the item in the list?
+  		lumis[row.value.lumi] = true;
+  	}
   }
   send('}');
 }
-
-
-//{
-//	'1': [[1, 33], [35, 35], [37, 47], [49, 75], [77, 130], [133, 136]],
-//	'2': [[1,45],[50,80]]
-//	}
-// I used double quotes
-
-//http://127.0.0.1:5984/dqis/_design/dqis/_list/crab/crab?group=true
-//for proper examle - shoud be 2 documents with same run, but differen lumi
--- src/couchapp/dqis/lists/search.js
+++ src/couchapp/dqis/lists/search.js
@@ -4,20 +4,27 @@ function(head, req){
   send('[');
   var body = JSON.parse(req.body);
   send('\n');
+  var first;
+  first = 0;
   while(row = getRow()) {
   	var sendRow;
-  	sendRow = false;
+  	sendRow = true;
+  	// If there's a bfield requested it better match the doc, else we don't care
   	if (body.bfield) {
-  		sendRow = (body.bfield == row.value.bfield);
+  		sendRow = (parseInt(body.bfield) == parseInt(row.value.bfield));
   	}
+  	// If we get a map we'd better make sure the doc matches the keys
   	if (body.map) {
   		for (key in body.map) {
-  			sendRow = (body.map[key] == row.value.map[key]);
+  			sendRow = sendRow & (body.map[key] == row.value.map[key])
   		}
   	}
   	if (sendRow == true) {
+	    if (first > 1) {
+		    send(', \n');
+		  }
   		send(toJSON(row));
-  		send(',');
+      first += 1;
   	}
   }
   send(']');
